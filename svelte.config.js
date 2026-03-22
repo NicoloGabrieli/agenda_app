40dfe7f05c3471746ee39728911fafc2bd6d315a
NicoloGabrieli <nicologabrieli8@gmail.com>
2026-03-22 10:47:27 +0100

    deploy: switch project to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html'
-		}),
-		paths: {
-			base: '/agenda_app'
-		}
+		adapter: adapter()
 	}
 };
